27932c6213d20f6615ef1fbd8478c34692976661
Siddesh Gavhane <siddeshwer90@gmail.com>
2024-09-19 23:17:03 +0530

    Add Multiple PING response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,7 +20,13 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String pingInput = bufferedReader.readLine();
+            while (pingInput != null){
+                if(pingInput.equals("PING")){
+                    clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                }
+            }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
